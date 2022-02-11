dfa1b974333c16864f1db3423432fab307c7b3b6
tusqasi <2002.tushar.kuntawar@gmail.com>
2022-02-12 00:17:07 +0530

    ✨ Now talks with kotlin
    Get inclination raw data from kotlin

--- android/app/src/main/kotlin/com/example/pothole_detector/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/pothole_detector/MainActivity.kt
@@ -1,6 +1,57 @@
 package com.example.pothole_detector
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorManager
+import android.os.Build
+import android.os.Bundle
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.EventChannel
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+    private val methodChannelName: String = "method_channel"
+    private val eventChannelName: String = "event_channel"
+
+    private lateinit var methodChannel: MethodChannel
+    private lateinit var eventChannel: EventChannel
+    private lateinit var sensorManager: SensorManager
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        GeneratedPluginRegistrant.registerWith(FlutterEngine(this))
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+                eventChannel =
+                    EventChannel(flutterEngine?.dartExecutor?.binaryMessenger, eventChannelName)
+
+                eventChannel.setStreamHandler(
+                    object : EventChannel.StreamHandler {
+                        override fun onCancel(p0: Any?) {}
+                        override fun onListen(p0: Any?, p1: EventChannel.EventSink?) {
+                            if (p1 != null) {
+                                    sensorManager.registerListener(
+                                        MySensorListener(p1),
+                                        sensorManager.getSensorList(Sensor.TYPE_GAME_ROTATION_VECTOR)[0],
+                                        1000
+                                    )
+                            }
+                        }
+                    }
+                )
+    }
 }
+
+
+class MySensorListener(eventSink: EventChannel.EventSink) : SensorEventListener {
+    private val _eventSink: EventChannel.EventSink = eventSink
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (event != null)
+            _eventSink.success(event.values)
+    }
+}
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-// import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/foundation.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
 import 'package:pothole_dectector/drawer.dart';
@@ -14,6 +15,18 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  /// Platform channels help talk with native andriod apis
+  final EventChannel _eventChannel = const EventChannel("event_channel");
+  final MethodChannel _methodChannel = const MethodChannel("method_channel");
+
+  /// Inclination of the phone
+  Map<String, double> inclination = {
+    "x": 0,
+    "y": 0,
+    "z": 0,
+    "f": 0,
+  };
+
   /// Control and access the map with [mapController].
   late GoogleMapController mapController;
 
@@ -30,7 +43,20 @@ class _MyAppState extends State<MyApp> {
   @override
   void initState() {
     super.initState();
+    _eventChannel.receiveBroadcastStream().listen(_onData, onError: _onError);
     setCustomMarker();
+  }
+
+  void _onError(dynamic event) {}
+  void _onData(dynamic event) {
+    setState(() {
+      inclination["x"] = event[0];
+      inclination["y"] = event[1];
+      inclination["z"] = event[2];
+      if (event.length > 4) {
+        inclination["f"] = event[3];
+      }
+    });
   }
 
   /// Convert the [AssetImage] to [BitmapDescriptor]
@@ -143,6 +169,14 @@ class _MyAppState extends State<MyApp> {
               ),
               buildingsEnabled: true,
               markers: _markers,
+            ),
+            Center(
+              child: Text(
+                'X: ${inclination["x"]?.toStringAsFixed(5)}\n'
+                'Y: ${inclination["y"]?.toStringAsFixed(5)}\n'
+                'Z: ${inclination["z"]?.toStringAsFixed(5)}\n'
+                'f: ${inclination["f"]?.toStringAsFixed(5)}\n',
+              ),
             ),
             Positioned(
               bottom: 50,
